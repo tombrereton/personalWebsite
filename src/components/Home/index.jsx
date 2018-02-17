a2791a1e2d7813d6f4c305c03b6ab3b9024ef7c6
tombrereton <tbrereton@gmail.com>
2018-02-17 12:04:32 +0000

    Update personal website to modular structure

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'normalize.css';
-import 'styles/index.scss';
+import './style.scss';
 
 const Home = () => (
   <div className='App'>
     <div>
-      <h1>It Works! Hello</h1>
+      <h1>It Works!</h1>
       <p>This React project just works including <span className="redBg">module</span> local styles.</p>
       <p>Enjoy!</p>
     </div>
