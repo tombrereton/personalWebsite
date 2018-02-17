d3b40e70257ff7e4fed864c50722d4f6b4ce7adc
tombrereton <tbrereton@gmail.com>
2018-02-17 09:59:39 +0000

    Refactor project to suit modular components

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import 'normalize.css';
-import 'styles/index.scss';
+import Home from './components/Home';
 
 const App = () => (
-  <div className='App'>
-    <div>
-      <h1>It Works!</h1>
-      <p>This React project just works including <span className="redBg">module</span> local styles.</p>
-      <p>Enjoy!</p>
-    </div>
-  </div>
+  <Home />
 );
 
 export default App;
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'normalize.css';
+import 'styles/index.scss';
+
+const Home = () => (
+  <div className='App'>
+    <div>
+      <h1>It Works! Hello</h1>
+      <p>This React project just works including <span className="redBg">module</span> local styles.</p>
+      <p>Enjoy!</p>
+    </div>
+  </div>
+);
+
+export default Home;
