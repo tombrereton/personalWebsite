0959b90eb8fa0470b386b6e407ec715b1eb793e4
tombrereton <tbrereton@gmail.com>
2018-03-02 14:48:28 +0000

    Add contact page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from '../Layout';
-import './style.css';
+import './style.scss';
 
 const Contact = () => (
   <Layout>
     <div className="contactContainer">
-      <p>Contact Page</p>
+      <form method="post">
+        <input type="hidden" name="form-name" value="contact" />
+          <label><p>Your Name: </p><input type="text" name="name" /></label>
+          <label><p>Your Email: </p><input type="email" name="email" /></label>
+          <label><p>What Is Your Project: </p><textarea name="message"></textarea></label>
+          <button type="submit">SEND</button>
+      </form>
     </div>
   </Layout>
 );
