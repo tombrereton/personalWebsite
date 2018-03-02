2df38e000ede8fdce1754f19f358a5b5a1c0428d
tombrereton <tbrereton@gmail.com>
2018-03-02 12:12:08 +0000

    Add google analytics

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
 import App from './components/App'
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-114987249-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
   <AppContainer>
